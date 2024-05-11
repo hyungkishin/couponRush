91e005b3fce19d63019b9f5a462fbbfa7ac1bba8
hyungkishin <tkaqkeldk@nate.com>
2024-05-12 01:10:51 +0900

    feat: 한명당 한개의 쿠폰만 발급받기

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,18 @@ public class ApplyService {
 
     private final CouponCreateProducer couponCreateProducer;
 
+    private final AppliedUserRepository appliedUserRepository;
+
     @Transactional
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        // 추가된 갯수가 1이 아닌경우, 이미 쿠폰을 발급 받은 사용자라 간주하고 비즈니스 로직을 수행하지 않는다.
+        if (apply != 1) {
+            return;
+        }
+
+        // 쿠폰 최초 발급 유저라 간주하고 발급 은 진행한다.
         Long count = couponCountRepository.increment();
 
         if (count > 100) {
--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -74,4 +74,35 @@ class ApplyServiceTest {
         assertThat(count).isEqualTo(100);  // 비즈니스 로직에서 제한했던 쿠폰 응모수 100 개와 일치하는지 검증
     }
 
+    @Test
+    public void 한명당_한개의쿠폰만_발급() throws InterruptedException {
+        int threadCount = 1000;  // 실제와 유사한 환경을 모방하도록 스레드 수 조정
+
+        // multiThread (병렬 작업을 도와주는 Java api)
+        ExecutorService executorService = Executors.newFixedThreadPool(32); // 연결 풀 크기에 맞춘 스레드 풀 크기
+
+        // 모든 요청이 끝날때 까지 기다림 ( 다른 스레드에서 수행하는 작업을 기다려주도록 도와주는 class )
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();  // 모든 스레드의 작업 완료를 기다림
+
+        Thread.sleep(10000);
+        executorService.shutdown();  // 스레드 풀 종료
+
+        long count = couponRepository.count();  // 모든 적용이 완료된 후에 쿠폰 수 검사
+
+        assertThat(count).isEqualTo(1);  // 비즈니스 로직에서 제한했던 쿠폰 응모수 100 개와 일치하는지 검증
+    }
+
 }
